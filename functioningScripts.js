dd750b339d43f89827c7f6cb07b04bfb7f46ea02
John Weber <johnhenryweber@gmail.com>
2019-09-03 18:59:29 -0700

    Future Outline

--- functioningScripts.js
+++ functioningScripts.js
@@ -2,7 +2,7 @@ const apiScript = require('./apiScripts.js');
 
 apiScript.getSongkickLocation('Portland,OR');
 
-apiScript.getSongkickShows(12283, '2019-09-06', '2019-09-07', 1);
+apiScript.getSongkickShows(12283, '2019-09-08', '2019-09-10', 1);
 
 apiScript.getYelpInformation(45.52335, -122.6765);
 
